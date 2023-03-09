2e6925dc5140429407614001f67cfbab42e69032
dodozee <104132677+dodozee@users.noreply.github.com>
2023-03-10 00:22:59 +0900

    [Bronze V 2743] Title: 단어 길이 재기, Time: 200 ms, Memory: 17624 KB -NKLCBHub

---
+++ #2743 단어 길이 재기/단어 길이 재기.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+import java.io.*;
+
+public class Main{ //가천대 컴공 20학번 화이팅~~
+    public static void main(String[] args) throws IOException {
+        Scanner s = new Scanner(System.in);
+        String str1 = s.next();
+        System.out.println(str1.length()); //String 객체는 length() 매서드를 사용해 문자열의 길이를 알 수 있다.
+
+    }
+}

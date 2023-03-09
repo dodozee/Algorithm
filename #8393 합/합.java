7b4fc351b6729227115d944eb8b9edd4a4053afa
dodozee <104132677+dodozee@users.noreply.github.com>
2023-03-10 00:24:59 +0900

    [Bronze V 8393] Title: 합, Time: 208 ms, Memory: 17708 KB -NKLCBHub

---
+++ #8393 합/합.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        int a=0;
+        for (int i = 1; i <= n; i++) {
+            a = a + i;
+        }
+        System.out.println(a);
+
+    }
+}

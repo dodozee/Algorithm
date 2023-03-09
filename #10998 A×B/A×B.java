4e4b5da3b3da4c5178ac9ca0db61bdd13906f570
dodozee <104132677+dodozee@users.noreply.github.com>
2023-03-10 00:31:16 +0900

    [Bronze V 10998] Title: A×B, Time: 208 ms, Memory: 17744 KB -NKLCBHub

---
+++ #10998 A×B/A×B.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] argv){
+        int A,B; //두 정수를 받을 변수
+        Scanner scan = new Scanner(System.in); //객체 지정한다
+
+
+        A = scan.nextInt();
+        B = scan.nextInt();
+
+
+        System.out.println(A*B);
+
+
+
+    }
+}

54f7554d7c1bc27610a372a6e530394b903c2b20
dodozee <104132677+dodozee@users.noreply.github.com>
2023-03-10 00:28:30 +0900

    [Bronze V 18108] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 204 ms, Memory: 17736 KB -NKLCBHub

---
+++ #18108 1998년생인 내가 태국에서는 2541년생?!/1998년생인 내가 태국에서는 2541년생？！.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] argv){
+
+        Scanner scan = new Scanner(System.in); //객체 지정한다
+        int i = scan.nextInt();
+
+        int j = i -543;
+    System.out.println(j);
+
+
+}
+}

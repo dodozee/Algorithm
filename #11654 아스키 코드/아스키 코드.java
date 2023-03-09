39db28ff312e289a7d4c82906a949937eb8a9bd3
dodozee <104132677+dodozee@users.noreply.github.com>
2023-03-10 00:23:20 +0900

    [Bronze V 11654] Title: 아스키 코드, Time: 196 ms, Memory: 17560 KB -NKLCBHub

---
+++ #11654 아스키 코드/아스키 코드.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+import java.io.*;
+
+public class Main{ //가천대 컴공 20학번 화이팅~~
+    public static void main(String[] args) throws IOException {
+        Scanner s = new Scanner(System.in);
+        String a = s.next();
+        System.out.println(a.charAt(0)-0);//charAt()뒤에 +0 이나 -0을하면 아스키코드로 출력이 가능.!!!
+    }
+}

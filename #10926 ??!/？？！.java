efb227cce5984dd4b8e8d743a40f1fd1703927e6
dodozee <104132677+dodozee@users.noreply.github.com>
2023-03-10 00:28:19 +0900

    [Bronze V 10926] Title: ??!, Time: 208 ms, Memory: 17752 KB -NKLCBHub

---
+++ #10926 ??!/？？！.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] argv){
+
+        Scanner scan = new Scanner(System.in); //객체 지정한다
+        String Id2="joonas";
+
+        String Id = scan.nextLine(); //Id에 문자열 입력
+
+        scan.close();
+
+
+            System.out.println(Id + "??!");
+        
+
+    }
+}

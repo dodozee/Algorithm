9da54f4228257661fd956ebd0cd12b85f3d79d3b
dodozee <104132677+dodozee@users.noreply.github.com>
2023-03-13 03:17:43 +0900

    [Bronze V 25083] Title: 새싹, Time: 120 ms, Memory: 13880 KB -NKLCBHub

---
+++ #25083 새싹/README.md
@@ -0,0 +1,15 @@
+# <img width="20px"  src="https://d2gd6pc034wcta.cloudfront.net/tier/1.svg" class="solvedac-tier"> [새싹](https://www.acmicpc.net/problem/25083) 
+
+| 제출 번호 | 닉네임 | 채점 결과 | 메모리 | 시간 | 언어 | 코드 길이 |
+|---|---|---|---|---|---|---|
+|57266959|jbin3031|맞았습니다!! |13880KB|120ms|Java 11|391B|
+
+## 문제
+<p>아래 예제와 같이 새싹을 출력하시오.</p>
+
+## 입력
+<p>입력은 없다.</p>
+
+## 출력
+<p>새싹을 출력한다.</p>
+
---
+++ #25083 새싹/새싹.java
@@ -0,0 +1,12 @@
+public class Main { //두지 화이팅! 가천 컴공 화이팅!
+	public static void main(String[] args) {
+		 String flower = "         ,r'\"7\n"    + 	
+				         "r`-_   ,'  ,/\n"      + 		
+				         " \\. \". L_r'\n"      + 		
+				         "   `~\\/\n"           + 			
+				         "      |\n"            + 			
+				         "      |"              ;
+ 
+		System.out.print(flower);
+	}
+}

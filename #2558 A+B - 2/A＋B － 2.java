b18456081b32a487f529eae0a14ea7cdeb24bbb1
두지 <104132677+dodozee@users.noreply.github.com>
2023-06-08 21:47:40 +0900

    [Bronze V 2558] Title: A+B - 2, Time: 212 ms, Memory: 17664 KB -NKLCBHub

---
+++ #2558 A+B - 2/A＋B － 2.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main { //두두지 화이팅!!
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int A = scanner.nextInt();
+        int B = scanner.nextInt();
+
+        int sum = A + B;
+
+        System.out.println(sum);
+    }
+}

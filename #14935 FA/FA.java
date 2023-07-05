31084f4319e3cd9ebd4134e437dc08ff842dfb56
두지 <104132677+dodozee@users.noreply.github.com>
2023-07-05 22:28:53 +0900

    [Bronze IV 14935] Title: FA, Time: 200 ms, Memory: 17628 KB -NKLCBHub

---
+++ #14935 FA/FA.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String x = sc.nextLine();
+		sc.close();
+
+		System.out.println("FA");
+	}
+
+}

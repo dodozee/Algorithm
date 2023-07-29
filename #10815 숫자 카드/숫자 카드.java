2bc7497bcfbcd0052b852af9d52edcf03bc0dd24
두지 <104132677+dodozee@users.noreply.github.com>
2023-07-29 23:21:39 +0900

    [Silver V 10815] Title: 숫자 카드, Time: 1316 ms, Memory: 103764 KB -NKLCBHub

--- #10815 숫자 카드/숫자 카드.java
+++ #10815 숫자 카드/숫자 카드.java
@@ -1,44 +1,56 @@
-import java.util.*;
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
-public class Main { //갓천대!!!포에버!! 나라사랑가천사랑동기사랑
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt(); // 상근이가 가지고 있는 숫자 카드의 개수
-        int[] cards = new int[n]; // 숫자 카드 배열
-        for (int i = 0; i < n; i++) {
-            cards[i] = sc.nextInt();
-        }
-        Arrays.sort(cards); // 이진 탐색을 위한 정렬
+public class Main {//박두지 화이팅!!
+    static int N, M;
+    static int arr[];
 
-        int m = sc.nextInt(); // 구해야 할 숫자 카드의 개수
-        int[] nums = new int[m]; // 구해야 할 숫자 카드 배열
-        for (int i = 0; i < m; i++) {
-            nums[i] = sc.nextInt();
-        }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        for (int i = 0; i < m; i++) {
-            int target = nums[i]; // 구해야 할 숫자 카드
-            int left = 0; // 이진 탐색을 위한 시작 인덱스
-            int right = n - 1; // 이진 탐색을 위한 끝 인덱스
-            boolean found = false; // 숫자 카드를 찾았는지 여부
+        N = Integer.parseInt(br.readLine());
+        arr = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-            while (left <= right) {
-                int mid = (left + right) / 2; // 중간 인덱스
-                if (cards[mid] == target) { // 숫자 카드를 찾은 경우
-                    found = true;
-                    break;
-                } else if (cards[mid] > target) { // 찾으려는 숫자 카드가 중간 값보다 작은 경우
-                    right = mid - 1;
-                } else { // 찾으려는 숫자 카드가 중간 값보다 큰 경우
-                    left = mid + 1;
-                }
+        for(int i = 0 ; i < N; i++)
+            arr[i] = Integer.parseInt(st.nextToken());
+
+        Arrays.sort(arr);
+
+        M = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+
+        for(int i = 0 ; i < M; i++) {
+            int num = Integer.parseInt(st.nextToken());
+            if(binarySearch(num)){
+                bw.write("1 ");
             }
-
-            if (found) {
-                System.out.print("1 ");
-            } else {
-                System.out.print("0 ");
+            else {
+                bw.write("0 ");
             }
         }
+
+        bw.close();
+        br.close();
+    }
+    private static boolean binarySearch(int num) {
+        int left = 0;
+        int right = N - 1;
+
+        while(left <= right){
+            int midIndex = (left + right) / 2;
+            int mid = arr[midIndex];
+
+            if(num < mid) right = midIndex - 1;
+            else if(num > mid) {
+                left = midIndex + 1;
+            }
+            else {
+                return true;
+            }
+        }
+        return false;
     }
 }

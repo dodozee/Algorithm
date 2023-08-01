5332b79ed0783b2cb308c5774c6352b58dcc6049
두지 <104132677+dodozee@users.noreply.github.com>
2023-08-01 20:46:57 +0900

    [Bronze V 2741] Title: N 찍기, Time: 1124 ms, Memory: 31200 KB -NKLCBHub

---
+++ #2741 N 찍기/N 찍기.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.io.*;
+
+public class Main{
+public static void main(String[] arg){
+    Scanner s = new Scanner(System.in);
+    int n = Integer.parseInt(s.nextLine());
+    for(int i=1; i<=n; i++){
+        System.out.println(i);
+    }
+}
+}

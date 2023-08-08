17853a338d9e40ba629674f3e02b9693be46e5f4
두지 <104132677+dodozee@users.noreply.github.com>
2023-08-08 20:52:27 +0900

    [Bronze III 2442] Title: 별 찍기 - 5, Time: 420 ms, Memory: 18668 KB -NKLCBHub

---
+++ #2442 별 찍기 - 5/별 찍기 － 5.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {//박두지 화이팅~~
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        for (int i = 1; i <= N; i++) {
+            // 왼쪽 공백 출력
+            for (int j = 0; j < N - i; j++) {
+                System.out.print(" ");
+            }
+            // 별 출력
+            for (int j = 0; j < 2 * i - 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println(); // 줄바꿈
+        }
+
+        sc.close();
+    }
+}

9214bf0b461dd1978afd9533473820c2be9d0ab6
parkjeongbin <104132677+dodozee@users.noreply.github.com>
2023-10-14 22:48:58 +0900

    [Bronze V 15680] Title: 연세대학교, Time: 204 ms, Memory: 17552 KB -NKLCBHub

---
+++ #15680 연세대학교/연세대학교.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {//박두지 화이팅!!
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        
+        if (N == 0) {
+            System.out.println("YONSEI");
+        } else if (N == 1) {
+            System.out.println("Leading the Way to the Future");
+        }
+    }
+}

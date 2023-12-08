22895a724c25fd0023480ff45fd9c2e29961a3e6
parkjeongbin <104132677+dodozee@users.noreply.github.com>
2023-12-08 22:56:54 +0900

    [Silver V 11931] Title: 수 정렬하기 4, Time: 2532 ms, Memory: 169516 KB -NKLCBHub

---
+++ #11931 수 정렬하기 4/수 정렬하기 4.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {//박두지 화이팅!!!!
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        int n = Integer.parseInt(st.nextToken());
+        Integer arr[] = new Integer[n];
+
+        for(int i = 0; i < n; i++){
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        
+        Arrays.sort(arr, Collections.reverseOrder());
+		
+		for (Integer i : arr) 
+			bw.write(i+"\n"); 
+            
+        bw.flush();
+    }
+}

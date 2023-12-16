7fd4503711c065a52926012eeb334ef3216d17bd
parkjeongbin <104132677+dodozee@users.noreply.github.com>
2023-12-16 23:34:52 +0900

    [Bronze I 1259] Title: 팰린드롬수, Time: 116 ms, Memory: 13956 KB -NKLCBHub

---
+++ #1259 팰린드롬수/팰린드롬수.java
@@ -0,0 +1,22 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));         
+        while (true) {            
+            String str = br.readLine();            
+            Boolean check = true;             
+            if(str.equals("0")) 
+                break;             
+            for (int i = 0; i < str.length()/2; i++) {                
+                if (str.charAt(i) != str.charAt(str.length() - 1 - i)) 
+                    check = false;            
+            }
+            if(check){
+                System.out.println("yes");
+            } else {
+                System.out.println("no");
+            }        
+        }    
+    }
+}

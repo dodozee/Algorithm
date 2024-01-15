bc69a56f1a268073bfa82fa0b3a54d94c73c29ff
parkjeongbin <104132677+dodozee@users.noreply.github.com>
2024-01-15 13:41:05 +0900

    [Bronze V 27331] Title: 2 桁の整数 (Two-digit Integer), Time: 208 ms, Memory: 17648 KB -NKLCBHub

---
+++ #27331 2 桁の整数 (Two-digit Integer)/2 桁の整数 （Two－digit Integer）.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {//박두지 화이팅
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int A = scanner.nextInt();
+        int B = scanner.nextInt();
+
+        int result = A * 10 + B;
+        System.out.println(result);
+
+    }
+}

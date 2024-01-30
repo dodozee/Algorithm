68b98a246ed2ba0d994629f9d008e5a5c86c4876
parkjeongbin <104132677+dodozee@users.noreply.github.com>
2024-01-30 20:56:56 +0900

    [Bronze V 8370] Title: Plane, Time: 212 ms, Memory: 17588 KB -NKLCBHub

---
+++ #8370 Plane/Plane.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {//박두지 화이팅!!!
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N1 = sc.nextInt();
+        int K1 = sc.nextInt();
+        int N2 = sc.nextInt();
+        int K2 = sc.nextInt();
+        System.out.println(N1 * K1 + N2 * K2);
+    }
+}

15a415ef637af09f762f364f2e00b1499e140b37
parkjeongbin <104132677+dodozee@users.noreply.github.com>
2024-02-12 23:34:40 +0900

    [Bronze V 20254] Title: Site Score, Time: 200 ms, Memory: 17608 KB -NKLCBHub

---
+++ #20254 Site Score/Site Score.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int U = sc.nextInt();
+        int T = sc.nextInt();
+        int Uo = sc.nextInt();
+        int To = sc.nextInt();
+        System.out.print(56*U + 24*T + 14*Uo + 6 *To);
+    }
+}

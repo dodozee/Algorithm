fd33ef1f5f30d7d067d9a6a8b4b7b3b83efa668b
parkjeongbin <104132677+dodozee@users.noreply.github.com>
2024-04-10 00:00:32 +0900

    [Gold II 12738] Title: 가장 긴 증가하는 부분 수열 3, Time: 568 ms, Memory: 121640 KB -NKLCBHub

---
+++ #12738 가장 긴 증가하는 부분 수열 3/가장 긴 증가하는 부분 수열 3.java
@@ -0,0 +1,52 @@
+import java.io.*;
+import java.util.*;
+/**
+ *
+ *
+ */
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));//입력
+
+        int N = Integer.parseInt(br.readLine());
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int[] arr = new int[N];
+        int[] LIS = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        LIS[0] = arr[0];
+        int lengthOfLIS = 1;
+
+        for (int i = 1; i < N; i++) {
+            int key = arr[i];
+
+            // LIS 배열의 마지막 값보다 key값이 크다면
+
+            if (LIS[lengthOfLIS - 1] < key) {
+                lengthOfLIS++;
+                LIS[lengthOfLIS - 1] = key;
+            }else{
+                //Lower Bound 방식으로 key값을 찾아서 변경
+                int lo = 0;
+                int hi = lengthOfLIS;
+
+                while(lo < hi){
+                    int mid = (lo + hi) / 2;
+                    if (LIS[mid] < key) {
+                        lo = mid + 1;
+                    }else{
+                        hi = mid;
+                    }
+                }
+                LIS[hi] = key;
+            }
+        }
+
+        System.out.println(lengthOfLIS);
+    }
+}

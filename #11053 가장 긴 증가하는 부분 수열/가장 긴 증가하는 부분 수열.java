08f4e53bb5e4e8c6674306c5be1d8fbe67b70bc3
parkjeongbin <104132677+dodozee@users.noreply.github.com>
2024-04-09 18:45:06 +0900

    [Silver II 11053] Title: 가장 긴 증가하는 부분 수열, Time: 140 ms, Memory: 14348 KB -NKLCBHub

--- #11053 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
+++ #11053 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
@@ -2,17 +2,18 @@ import java.io.*;
 import java.util.*;
 
 /**
- * dp
+ * bottom-up
  *
  */
 
 public class Main {
+    static int N;
     static int[] seq;
     static int[] dp;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));//입력
 
-        int N = Integer.parseInt(br.readLine());
+        N = Integer.parseInt(br.readLine());
 
         seq = new int[N];
         dp = new int[N];
@@ -24,30 +25,26 @@ public class Main {
         }
 
         //모든 위치에서 LIS 탐색
-        for (int i = 0; i < N; i++) {
-            LIS(i);
-        }
+        LIS();
 
         int max = dp[0];
+
         for (int i = 1; i < N; i++) {
             max = Math.max(max, dp[i]);
         }
         System.out.println(max);
     }
 
-    private static int LIS(int N) {
-        //만약 탐색하지 않던 위치의 경우 1로 초기화
-        if (dp[N] == 0) {
-            dp[N] = 1;
+    private static void LIS() {
+        for (int i = 0; i < N; i++) {
+            dp[i] = 1;
 
-            //N 이전의 수들을 탐색
-            for (int i = N - 1; i >= 0; i--) {
-                //이전의 노드 중 seq[N]이 큰 경우
-                if (seq[i] < seq[N]) {
-                    dp[N] = Math.max(dp[N], LIS(i) + 1);
+            for (int j = 0; j < i; j++) {
+                if (seq[j] < seq[i] && dp[i] < dp[j] + 1) {
+                    dp[i] = dp[j] + 1;
                 }
             }
         }
-        return dp[N];
+
     }
 }

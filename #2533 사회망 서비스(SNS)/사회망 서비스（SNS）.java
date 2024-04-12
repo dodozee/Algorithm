0562b304c23d596f2f0b7c0942fc3fa2ae9a4612
parkjeongbin <104132677+dodozee@users.noreply.github.com>
2024-04-13 02:20:32 +0900

    [Gold III 2533] Title: 사회망 서비스(SNS), Time: 1056 ms, Memory: 274016 KB -NKLCBHub

--- #2533 사회망 서비스(SNS)/사회망 서비스（SNS）.java
+++ #2533 사회망 서비스(SNS)/사회망 서비스（SNS）.java
@@ -6,53 +6,60 @@ import java.util.*;
  * dp
  * 얼리 아답터 : 어떤 새로운 아이디어를 먼저 받아들인 사람을 일컫는다.
  * 얼리 아답터가 아닌 사람들은 "자신의 주변의 모든 친구들이 얼리아답터일 때만" 새로운 아이디어를 받아들인다.
- *
+ * <p>
  * 결국 어떤 아이디어를 SNS 상에서 전파하고자 할 때, "최소한의 얼리 아답터를 확보"하여 모든 사람에게 전파하는 것이 목표이다.
- *
+ * <p>
  * 조건 : 모든 두 정점 사이에 이들은 잇는 경로가 존재는 하지만 사이클은 존재하지 않는 경우만 고려한다.
  */
 public class Main {
+    static class Node{
+        int n ;// 노드 번호
+        Node next; // 다음 노드
+        Node(int n, Node next){
+            this.n = n;
+            this.next = next;
+        }
+    }
     static int n;
-    static int[][] dp;
     static boolean[] visited;
-    static List<Integer>[] graph;
+    static Node[] tree;
+    static int[][] dp; //dp : 해당 지점까지의 얼리어답터의 최소 인원수
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-        n = Integer.parseInt(br.readLine());
-        dp = new int[n+1][2];
-        visited = new boolean[n+1];
-        graph = new ArrayList[n+1];
 
-        for (int i = 1; i <= n; i++) {
-            graph[i] = new ArrayList<>();//인접리스트 초기화
-        }
+        n = Integer.parseInt(br.readLine());
+        dp = new int[n + 1][2];
+        visited = new boolean[n + 1];
+        tree = new Node[n + 1];
+
+        StringTokenizer st;
 
         for (int i = 1; i < n; i++) {
             st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            graph[a].add(b);
-            graph[b].add(a);
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            tree[start] = new Node(end, tree[start]);
+            tree[end] = new Node(start, tree[end]);
         }
 
+
         dfs(1);
-        System.out.println(Math.min(dp[1][0], dp[1][1]));
 
-
+        System.out.println(Math.min(dp[1][0], dp[1][1]));// 1번 노드가 얼리어답터인 경우와 아닌 경우 중 최소값 출력
     }
 
-    private static void dfs(int i) {
-        visited[i] = true;
-        dp[i][0] = 0; // i가 얼리 아답터가 아닌 경우
-        dp[i][1] = 1; // i가 얼리 아답터인 경우
+    static void dfs(int n){
+        visited[n] = true;
+        dp[n][0] = 0; // 해당 노드가 얼리어답터가 아닌 경우
+        dp[n][1] = 1; // 해당 노드가 얼리어답터인 경우
 
-        for (int child : graph[i]) {
-            if (!visited[child]) {
-                dfs(child);
-                dp[i][0] += dp[child][1]; // i가 얼리 아답터가 아닌 경우, 자식은 얼리 아답터여야 한다.
-                dp[i][1] += Math.min(dp[child][0], dp[child][1]); // i가 얼리 아답터인 경우, 자식은 얼리 아답터일 수도 있고 아닐 수도 있다.
+        for (Node next = tree[n]; next != null; next = next.next) {
+            if (!visited[next.n]) {
+                dfs(next.n);
+                dp[n][0] += dp[next.n][1]; // 해당 노드가 얼리어답터가 아닌 경우는 자식 노드가 얼리어답터인 경우만 가능
+                dp[n][1] += Math.min(dp[next.n][0], dp[next.n][1]); // 해당 노드가 얼리어답터인 경우는 자식 노드가 얼리어답터인 경우와 아닌 경우 중 최소값만큼만 가능
             }
         }
     }
+
 }

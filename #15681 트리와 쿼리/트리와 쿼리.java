0204a454758f4e495ce719bc738b841e22cf7195
parkjeongbin <104132677+dodozee@users.noreply.github.com>
2024-04-13 03:17:45 +0900

    [Gold V 15681] Title: 트리와 쿼리, Time: 688 ms, Memory: 85436 KB -NKLCBHub

--- #15681 트리와 쿼리/트리와 쿼리.java
+++ #15681 트리와 쿼리/트리와 쿼리.java
@@ -8,10 +8,8 @@ import java.util.*;
  */
 public class Main {
     static ArrayList<Integer>[] list; //용도 : 입력받은 트리
-    static ArrayList<Integer>[] tree; //용도 : 트리
-    static int[] parent;
+    static boolean[] visited;
     static int[] dp;
-    static int[] size;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -21,15 +19,12 @@ public class Main {
         int R = Integer.parseInt(st.nextToken());
         int Q = Integer.parseInt(st.nextToken());
 
-        parent = new int[N + 1];
-        size = new int[N + 1];
-        dp = new int[N + 1];
         list = new ArrayList[N + 1];
-        tree = new ArrayList[N + 1];
+        dp = new int[N + 1];
+        visited = new boolean[N + 1];
 
         for (int i = 0; i < list.length; i++) {
             list[i] = new ArrayList<>();
-            tree[i] = new ArrayList<>();
         }
 
         for (int i = 1; i < N; i++) {
@@ -39,35 +34,37 @@ public class Main {
 
             list[u].add(v);
             list[v].add(u);
+
         }
 
-        makeTree(R, -1);
-        countSubTreeNode(R);
+        dfs(R);
+        StringBuffer sb = new StringBuffer();
 
-        for (int i = 0; i < Q; i++) {
+        while (Q--> 0) {
             int query = Integer.parseInt(br.readLine());
-            System.out.println(size[query]);
+
+            sb.append(dp[query] + "\n");
         }
 
+        System.out.println(sb.toString());
     }
 
-    private static void makeTree(int currentNode, int p) {
-        for(int node : list[currentNode]){
-            if(node == p) continue;
-            tree[currentNode].add(node);//트리에 추가
-            parent[node] = currentNode;//부모노드 저장
-            makeTree(node, currentNode);//재귀
+    private static int dfs(int now) {
+        if (dp[now] != 0) {
+            return dp[now];
         }
-    }
 
-    private static void countSubTreeNode(int currentNode) {
-        size[currentNode] = 1;//최소 자기 자신
+        visited[now] = true;
+        int count = 1;
 
-        for (int node : tree[currentNode]) {
-            countSubTreeNode(node);
-            size[currentNode] += size[node];//자식노드의 개수를 더해줌
+        for(int node : list[now]){
+            if (!visited[node]) {
+                count += dfs(node);
+            }
+            dp[now] = count;
+
         }
+
+        return dp[now];
     }
-
-
 }

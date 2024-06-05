db376e5d8fd80c27c8f0a3d7d358d17e7ca9439d
parkjeongbin <104132677+dodozee@users.noreply.github.com>
2024-06-05 14:51:11 +0900

    [Bronze V 28701] Title: 세제곱의 합, Time: 204 ms, Memory: 17692 KB -NKLCBHub

---
+++ #28701 세제곱의 합/세제곱의 합.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		int ans = 0;
+		for(int i = 1; i <= n; i++) {
+			ans += i;
+		}
+		System.out.println(ans);
+		System.out.println(ans * ans);
+		
+		ans = 0;
+		for(int i = 1; i <= n; i++) {
+			ans += i * i * i;
+		}
+		System.out.println(ans);
+	}
+}

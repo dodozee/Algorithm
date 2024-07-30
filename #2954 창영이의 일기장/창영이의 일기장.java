40b062216e0ec7df2fc731f549bec4694d9ec007
parkjeongbin <104132677+dodozee@users.noreply.github.com>
2024-07-30 22:53:00 +0900

    [Bronze II 2954] Title: 창영이의 일기장, Time: 124 ms, Memory: 14292 KB -NKLCBHub

---
+++ #2954 창영이의 일기장/창영이의 일기장.java
@@ -0,0 +1,13 @@
+import java.io.*;
+ 
+public class Main {
+ 
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+ 
+        str = str.replaceAll("(?:([a|e|i|o|u]{1})(p{1})([a|e|i|o|u{1}]))", "$1");
+        
+        System.out.println(str);
+    }
+}

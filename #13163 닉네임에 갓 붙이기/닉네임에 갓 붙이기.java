bfead0b8228464b2d34c72c88cae3a2ca1cc4471
parkjeongbin <104132677+dodozee@users.noreply.github.com>
2024-07-31 22:52:45 +0900

    [Bronze II 13163] Title: 닉네임에 갓 붙이기, Time: 132 ms, Memory: 14380 KB -NKLCBHub

---
+++ #13163 닉네임에 갓 붙이기/닉네임에 갓 붙이기.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+		
+        int size = Integer.parseInt(br.readLine());
+        
+        StringBuilder sb = new StringBuilder();
+        
+        while(size --> 0) {
+        	StringTokenizer st = new StringTokenizer(br.readLine());
+        	
+        	sb.append("god");
+        	st.nextToken();
+        	while(st.hasMoreTokens()) {
+        		sb.append(st.nextToken());
+        	}
+        	sb.append("\n");
+        }
+        System.out.print(sb);
+	}
+}

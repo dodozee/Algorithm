3ce136db8eecacf69d924b27f14a37a194c88414
parkjeongbin <104132677+dodozee@users.noreply.github.com>
2024-08-07 21:01:35 +0900

    [Bronze III 4740] Title: 거울, 오! 거울, Time: 100 ms, Memory: 13984 KB -NKLCBHub

---
+++ #4740 거울, 오! 거울/거울， 오！ 거울.java
@@ -0,0 +1,21 @@
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+        
+		while(true) {
+            
+			String str = br.readLine();
+			if(str.equals("***")) {
+				break;
+			}
+			for(int i=0;i<str.length();i++) {
+				sb.append(str.charAt((str.length()-1)-i));
+			}
+			sb.append("\n");
+		}
+		System.out.println(sb.toString());
+	}
+}

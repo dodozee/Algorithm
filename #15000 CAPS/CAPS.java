afc8f266dbc59316745b76fd8892c4586cbf2012
parkjeongbin <104132677+dodozee@users.noreply.github.com>
2024-08-13 22:40:49 +0900

    [Bronze V 15000] Title: CAPS, Time: 504 ms, Memory: 30852 KB -NKLCBHub

---
+++ #15000 CAPS/CAPS.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main {
+	
+	 public static void main(String[] args) {
+	      Scanner scan = new Scanner(System.in);
+	      
+	      String str = scan.nextLine();
+	      str = str.toUpperCase();
+	    
+	      System.out.println(str);
+	   }
+}

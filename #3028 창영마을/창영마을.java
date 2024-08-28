fd7ff47ec993a93374e1f947e7fe099d89911707
parkjeongbin <104132677+dodozee@users.noreply.github.com>
2024-08-28 16:58:55 +0900

    [Bronze II 3028] Title: 창영마을, Time: 96 ms, Memory: 14000 KB -NKLCBHub

---
+++ #3028 창영마을/창영마을.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String S = br.readLine();
+		ArrayList<String> list = new ArrayList<String>(Arrays.asList("O", "X", "X"));
+		
+		for(int i = 0; i < S.length(); i++) {
+			if(S.charAt(i) == 'A') {
+				Collections.swap(list, 0, 1);
+			}else if(S.charAt(i) == 'B') {
+				Collections.swap(list, 1, 2);
+			}else if(S.charAt(i) == 'C') {
+				Collections.swap(list, 0, 2);
+			}
+		}
+		System.out.println(list.indexOf("O") + 1);
+	}
+
+}

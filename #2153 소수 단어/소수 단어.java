5ffa878d62aa29b0d8134bc854e163f2a366b322
parkjeongbin <104132677+dodozee@users.noreply.github.com>
2024-09-10 14:28:27 +0900

    [Bronze II 2153] Title: 소수 단어, Time: 100 ms, Memory: 14072 KB -NKLCBHub

---
+++ #2153 소수 단어/소수 단어.java
@@ -0,0 +1,37 @@
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		String str = br.readLine();
+		int sum = 0;
+		boolean isPrime = false;
+
+		for (int i = 0; i < str.length(); i++) {
+			char c = str.charAt(i);
+			if (c >= 'a' && c <= 'z') {
+				sum += (int) c - 96;
+			} else if (c >= 'A' && c <= 'Z') {
+				sum += (int) c - 38;
+			}
+		}
+
+		for (int i = 2; i < sum; i++) {
+			if (sum % i == 0) {
+				isPrime = true;
+				break;
+			}
+		}
+
+		if (isPrime) {
+			bw.write("It is not a prime word.");
+		} else if(!isPrime){
+			bw.write("It is a prime word.");
+		}
+
+		bw.flush();
+        bw.close();
+	}
+}

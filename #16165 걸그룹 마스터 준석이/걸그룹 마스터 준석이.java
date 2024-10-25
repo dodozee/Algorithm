902020cf4e7f8d26cc02e51720e666fe73f18da5
parkjeongbin <104132677+dodozee@users.noreply.github.com>
2024-10-25 10:56:30 +0900

    [Silver III 16165] Title: 걸그룹 마스터 준석이, Time: 104 ms, Memory: 14208 KB -NKLCBHub

--- #16165 걸그룹 마스터 준석이/걸그룹 마스터 준석이.java
+++ #16165 걸그룹 마스터 준석이/걸그룹 마스터 준석이.java
@@ -1,43 +1,53 @@
-import java.io.BufferedReader;
+
 import java.io.*;
-import java.io.OutputStreamWriter;
 import java.util.*;
-import java.util.LinkedList;
-import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int N = Integer.parseInt(st.nextToken());
-		int M = Integer.parseInt(st.nextToken());
-		HashMap<String, String[]> team = new HashMap<String, String[]>();
-		HashMap<String, String> girl = new HashMap<String, String>();
-		while (N-->0) {
-			String tName = br.readLine();
-			int K = Integer.parseInt(br.readLine());
-			String[] arr = new String[K];
-			while (K-->0) {
-				String gName = br.readLine();
-				girl.put(gName, tName);
-				arr[K] = gName;
-			}
-			Arrays.sort(arr);
-			team.put(tName, arr);
-		}
-		while (M-->0) {
-			String tmp = br.readLine();
-			int pr = Integer.parseInt(br.readLine());
-			switch (pr) {
-			case 0 :
-				for (String s : team.get(tmp))
-					bw.write(s + "\n");
-				break;
-			case 1 :
-				bw.write(girl.get(tmp) + "\n");
-			}
-		}
-		bw.flush();		
-	}
+    private static final int DAY_OF_YEAR = 365;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        HashMap<String, String[]> team = new HashMap<>();
+        HashMap<String, String> girl = new HashMap<>();
+
+        while (n-- > 0) {
+            String teamName = br.readLine();
+            int k = Integer.parseInt(br.readLine());
+            String[] teamMembers = new String[k];
+
+            while (k-- > 0) {
+                String name = br.readLine();
+                teamMembers[k] = name;
+                girl.put(name, teamName);
+            }
+            Arrays.sort(teamMembers);//왜 정렬? -> 팀원 이름을 사전순으로 정렬하기 위해
+
+            team.put(teamName, teamMembers);
+
+
+        }
+        while (m-- > 0) {
+            String tmp = br.readLine();
+            int quiz = Integer.parseInt(br.readLine());
+
+            switch(quiz){
+                case 0:
+                    for (String name : team.get(tmp)) {
+                        System.out.println(name);
+                    }
+                    break;
+                case 1:
+                    System.out.println(girl.get(tmp));
+            }
+        }
+
+
+    }
 }
+
+

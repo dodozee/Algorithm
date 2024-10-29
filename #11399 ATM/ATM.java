f7f1cca561a7443259b36c18a1caa64a2aa83030
parkjeongbin <104132677+dodozee@users.noreply.github.com>
2024-10-30 07:07:01 +0900

    [Silver IV 11399] Title: ATM, Time: 112 ms, Memory: 14476 KB -NKLCBHub

--- #11399 ATM/ATM.java
+++ #11399 ATM/ATM.java
@@ -1,28 +1,27 @@
 import java.io.*;
-import java.util.Arrays;
+import java.util.*;
 
-public class Main { //나라사랑 가천사랑 동기사랑 !!!화티이!!!
-    public static void main(String[] args) throws IOException {
+/*
+분석
+
+ */
+public class Main {
+    public static void main(String[] args) throws IOException, InterruptedException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n =Integer.parseInt(br.readLine());
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        String[] arr1;
-        int[] arr2 = new int[n];
-        String str = br.readLine();
-
-        arr1 = str.split(" "); //" "스페이스 구간으로 쪼개서 String 배열에 저장하기
-
-        for (int i = 0; i < arr1.length; i++) {
-            arr2[i] = Integer.parseInt(arr1[i]); // String 배열에 있는 문자형 숫자를 숫자형으로 바꾸는 과정
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
         }
-        Arrays.sort(arr2); //Arrays 클래스로 간편하게 정렬하기~ 오름차순으로 정리하는게 포인트다.
+        Arrays.sort(arr);
+
         int sum = 0;
-        int result =0;
-        for(int i=0; i<arr2.length; i++) {
-           sum = sum + arr2[i];
-           result +=sum;
-//         로그 찍는 용!!!   System.out.println("i번 째 : " + i + " result :" +result);
+        for(int i = 0; i < n; i++) {
+            sum += arr[i]*(n-i);
+
         }
-        System.out.print(result);
+        System.out.println(sum);
     }
 }

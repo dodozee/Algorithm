a71e18985a0cdf2f9eaddeb8e56958b27930f747
parkjeongbin <104132677+dodozee@users.noreply.github.com>
2024-12-16 21:42:44 +0900

    [Bronze I] Title: XORXORXOR, Time: 108 ms, Memory: 14120 KB -BaekjoonHub

---
+++ 백준/Bronze/12833. XORXORXOR/XORXORXOR.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        final StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        final int numA = Integer.parseInt(st.nextToken());
+        final int numB = Integer.parseInt(st.nextToken());
+        final int numC = Integer.parseInt(st.nextToken()) % 2;
+
+        int ans = numA;
+        if(numC == 1){
+            ans = numA ^ numB;
+        }
+        System.out.println(ans);
+    }
+}

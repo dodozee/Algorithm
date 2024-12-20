2d5c51887e2828e51c7ea5467da08d2b6ef8436f
parkjeongbin <104132677+dodozee@users.noreply.github.com>
2024-12-20 17:27:10 +0900

    [Bronze II] Title: 카드게임, Time: 108 ms, Memory: 14276 KB -BaekjoonHub

---
+++ 백준/Bronze/10801. 카드게임/카드게임.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] aList = new int[10];
+        int[] bList = new int[10];
+
+        for (int i = 0; i < 10; i++) {
+            aList[i] = Integer.parseInt(st.nextToken());
+        }
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < 10; i++) {
+            bList[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int aCount = 0, bCount = 0, dCount = 0;
+
+        for (int i = 0; i < 10; i++) {
+            if (aList[i] > bList[i]) {
+                aCount++;
+            } else if (aList[i] < bList[i]) {
+                bCount++;
+            } else {
+                dCount++;
+            }
+        }
+        if (aCount == bCount) {
+            System.out.println("D");
+        } else if (aCount > bCount) {
+            System.out.println("A");
+        } else {
+            System.out.println("B");
+        }
+    }
+}

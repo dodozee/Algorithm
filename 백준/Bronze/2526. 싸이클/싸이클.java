ec151e6dba4c3a7041207969f7ddc8689e817733
parkjeongbin <104132677+dodozee@users.noreply.github.com>
2024-12-26 15:25:22 +0900

    [Bronze I] Title: 싸이클, Time: 112 ms, Memory: 14316 KB -BaekjoonHub

---
+++ 백준/Bronze/2526. 싸이클/싸이클.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int p = Integer.parseInt(st.nextToken());
+
+        int value = n;
+        Set<Integer> set1 = new HashSet<>();
+        Set<Integer> set2 = new HashSet<>();
+        while(true) {
+            value = (value * n) % p;
+            if(!set1.add(value)) {
+                if(!set2.add(value)) break;
+            }
+        }
+
+        bw.write(set2.size() + "");
+        bw.flush();
+    }
+}

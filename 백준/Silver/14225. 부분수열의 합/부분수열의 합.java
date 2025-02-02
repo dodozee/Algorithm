cb1a4c1fe16a97b280662f1a1f01c80029227795
parkjeongbin <104132677+dodozee@users.noreply.github.com>
2025-02-02 22:09:53 +0900

    [Silver I] Title: 부분수열의 합, Time: 120 ms, Memory: 16352 KB -BaekjoonHub

---
+++ 백준/Silver/14225. 부분수열의 합/부분수열의 합.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+  static int[] num;
+  static boolean[] isVisit;
+  static boolean[] isUse;
+  static int N;
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    N = Integer.parseInt(br.readLine());
+    num = new int[N];
+    isVisit = new boolean[N];
+    isUse = new boolean[100000*20 + 1];
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    for(int i=0; i<N; i++) {
+      num[i] = Integer.parseInt(st.nextToken());
+    }
+    dfs(0,0);
+    for(int i=1; i<isUse.length; i++) {
+      if(!isUse[i]) {
+        System.out.println(i);
+        break;
+      }
+    }
+  }
+  public static void dfs(int depth,int sum) {
+    if(depth == N) {
+      isUse[sum] = true;
+      return;
+    }
+    dfs(depth+1,sum+num[depth]);
+    dfs(depth+1,sum);
+  }
+}

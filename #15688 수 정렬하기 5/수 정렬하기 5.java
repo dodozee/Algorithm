1a66986bbd9d5b23e46d8904193912180e560d6c
parkjeongbin <104132677+dodozee@users.noreply.github.com>
2025-02-03 23:55:50 +0900

    [Silver V 15688] Title: 수 정렬하기 5, Time: 17952 ms, Memory: 106228 KB -NKLCBHub

---
+++ #15688 수 정렬하기 5/수 정렬하기 5.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+
+/*
+분석
+
+ */
+public class Main {
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        Arrays.sort(arr);
+
+        for(int i =0; i < n; i++){
+            sb.append(arr[i]).append("\n");
+        }
+
+        System.out.println(sb);
+
+    }
+}
+

4190087d018191e83863b6cc7a70f34bcad07bcf
parkjeongbin <104132677+dodozee@users.noreply.github.com>
2025-05-01 23:57:43 +0900

    [Silver V] Title: Base Conversion, Time: 108 ms, Memory: 14436 KB -BaekjoonHub

---
+++ 백준/Silver/11576. Base Conversion/Base Conversion.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(br.readLine());
+        Stack<Integer> stack = new Stack<>();
+
+        st = new StringTokenizer(br.readLine(), " ");
+
+        int sum = 0;
+
+        for (int i = m - 1; i >= 0; i--) {
+            int N = Integer.parseInt(st.nextToken());
+            sum += N * Math.pow(A, i);
+        }
+
+        while (sum != 0) {
+            stack.push(sum % B);
+            sum /= B;
+        }
+
+        while (!stack.isEmpty())
+            System.out.print(stack.pop() + " ");
+    }
+}

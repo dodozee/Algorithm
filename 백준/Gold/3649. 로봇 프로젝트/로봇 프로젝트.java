bea5a278c66c8e22eadf8ee5da8911bda8f2997b
parkjeongbin <104132677+dodozee@users.noreply.github.com>
2025-06-27 22:54:03 +0900

    [Gold V] Title: 로봇 프로젝트, Time: 2280 ms, Memory: 313956 KB -BaekjoonHub

---
+++ 백준/Gold/3649. 로봇 프로젝트/로봇 프로젝트.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String s = null;
+
+		while ((s = br.readLine()) != null) {
+			int size = Integer.parseInt(s) * 10000000;
+			int N = Integer.parseInt(br.readLine());
+			int[] list = new int[N];
+
+			for (int i = 0; i < N; i++) {
+				list[i] = Integer.parseInt(br.readLine());
+			}
+
+			Arrays.sort(list);
+
+			int left = 0, right = N - 1;
+			boolean flag = false;
+
+			while (left < right) {
+				int sum = list[left] + list[right];
+
+				if (sum == size) {
+					System.out.printf("yes %d %d\n", list[left], list[right]);
+					flag = true;
+					break;
+				} else if (sum > size) {
+					right--;
+				} else {
+					left++;
+				}
+			}
+
+			if (!flag) {
+				System.out.println("danger");
+			}
+			
+			s = null;
+		}
+	}
+}

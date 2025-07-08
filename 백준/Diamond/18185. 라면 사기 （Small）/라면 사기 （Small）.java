298ce443af7956ff8503f045d65c3a1007222008
parkjeongbin <104132677+dodozee@users.noreply.github.com>
2025-07-08 23:12:52 +0900

    [Diamond V] Title: 라면 사기 (Small), Time: 136 ms, Memory: 15244 KB -BaekjoonHub

---
+++ 백준/Diamond/18185. 라면 사기 （Small）/라면 사기 （Small）.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		String str;
+		StringTokenizer st;
+		
+		int n, i;
+		long b, c, ramen, cntB, cntC1 = 0, cntC2 = 0, ans = 0;
+		
+		str = br.readLine();
+		n = Integer.parseInt(str);
+		b = 3;
+		c = 2;
+		
+		str = br.readLine();
+		st = new StringTokenizer(str, " ");
+		if (b < c) {
+			c = b;
+		}
+		for (i = 0; i < n; i++) {
+			ramen = Long.parseLong(st.nextToken());
+			cntB = ramen - Math.min(ramen, cntC1);
+			ans += (b * cntB) + (c * Math.min(ramen, cntC1));
+			cntC2 = Math.min(cntC2, ramen);
+			cntC1 = cntC2 + cntB;
+			cntC2 = cntB;
+		}
+		
+		bw.write(Long.toString(ans));
+		
+		bw.flush();
+		bw.close();
+
+	}
+
+}

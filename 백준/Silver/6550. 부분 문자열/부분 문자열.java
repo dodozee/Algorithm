a9b1023cf13d42bdeb065ed7084ead0b7d70c892
parkjeongbin <104132677+dodozee@users.noreply.github.com>
2025-08-08 23:54:59 +0900

    [Silver V] Title: 부분 문자열, Time: 160 ms, Memory: 16140 KB -BaekjoonHub

---
+++ 백준/Silver/6550. 부분 문자열/부분 문자열.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	static String str;
+	static int idx;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		while((str=br.readLine())!=null){
+			st=new StringTokenizer(str);
+			String s=st.nextToken();
+			String t=st.nextToken();
+
+			idx=0;
+			for(int i=0;i<t.length();i++){
+				if(s.charAt(idx)==t.charAt(i)){ 
+					idx++;
+				}
+				if(idx == s.length()) 
+					break;
+			}
+
+			if(idx==s.length()){
+				System.out.println("Yes");
+			}
+
+			else{
+				System.out.println("No");
+			}
+		}
+	}
+}

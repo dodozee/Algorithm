ab039fad902222225337f9e986a4f0b38644d4e2
parkjeongbin <104132677+dodozee@users.noreply.github.com>
2025-08-19 23:54:20 +0900

    [Silver IV] Title: 귀찮아 (SIB), Time: 724 ms, Memory: 112488 KB -BaekjoonHub

---
+++ 백준/Silver/14929. 귀찮아 （SIB）/귀찮아 （SIB）.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt()+1;
+
+        int[] O = new int[N];
+        int[] A = new int[N];
+
+        for(int i=1; i<N; i++){
+            O[i] = sc.nextInt();
+            A[i] = A[i-1] + O[i];
+        }
+
+        long sum = 0;
+        for(int i=1; i<N; i++){
+            sum += A[i-1]*O[i];
+        }
+
+        System.out.println(sum);
+    }
+}

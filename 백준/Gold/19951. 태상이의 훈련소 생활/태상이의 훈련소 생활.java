b094c63c3b45063dfb344a947715474ec1e5ec50
parkjeongbin <104132677+dodozee@users.noreply.github.com>
2025-09-20 20:52:13 +0900

    [Gold V] Title: 태상이의 훈련소 생활, Time: 1244 ms, Memory: 310248 KB -BaekjoonHub

---
+++ 백준/Gold/19951. 태상이의 훈련소 생활/태상이의 훈련소 생활.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		int m = sc.nextInt();
+		
+		int[] arr = new int[n+1];
+		for(int i=1; i<arr.length; i++) {
+			arr[i] = sc.nextInt();
+		}
+		
+		int[] dp = new int[n+2];
+		for(int t=0; t<m; t++) {
+			int start = sc.nextInt();
+			int end = sc.nextInt();
+			int k = sc.nextInt();
+			
+			dp[start] = dp[start]+k;
+			dp[end+1] = dp[end+1]-k;
+		}
+		
+		StringBuilder sb = new StringBuilder();
+		for(int i=1; i<arr.length; i++) {
+			dp[i] = dp[i]+dp[i-1];
+			sb.append((arr[i]+dp[i])+ " ");
+		}
+		
+		System.out.println(sb.toString());
+		
+	}
+}

c7cbc3225850d27c169c0d91a02eb20574cd9302
parkjeongbin <104132677+dodozee@users.noreply.github.com>
2026-01-22 23:53:46 +0900

    [Silver IV] Title: 당근 키우기, Time: 108 ms, Memory: 14260 KB -BaekjoonHub

---
+++ 백준/Silver/20363. 당근 키우기/당근 키우기.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        long X = Long.parseLong(st.nextToken());
+        long Y = Long.parseLong(st.nextToken());
+
+        long extra = Math.min(X / 10, Y / 10);
+        long ans = X + Y + extra;
+
+        System.out.println(ans);
+    }
+}
